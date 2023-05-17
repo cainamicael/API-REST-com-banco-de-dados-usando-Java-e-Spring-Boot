049e2a6d324c521e2bf7c7f596662d7a9308ba7f
cainamicael <cainamicaeloficial@gmail.com>
2023-05-17 11:56:36 -0300

    add

---
+++ userdept/src/main/java/com/devsuperior/userdept/entities/Department.java
@@ -0,0 +1,29 @@
+package com.devsuperior.userdept.entities;
+
+public class Department {
+	
+	private long id;
+	private String name;
+	
+	public Department() {
+		
+	}
+	
+	public long getId() {
+		return id;
+	}
+	
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+
+}
